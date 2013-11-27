9303672ddca467075af895b01ff0d939135cc0b0
mc22ft <mc22ft@student.lnu.se>
2013-11-27 12:55:20 +0100

    mitt i testet

--- Whtiebox Test kod/UnitTestTry/UnitTest1.cs
+++ Whtiebox Test kod/UnitTestTry/UnitTest1.cs
@@ -6,44 +6,84 @@ namespace UnitTestTry
     [TestClass]
     public class TriagleUnitTest1
     {
-
+        /*
         [TestMethod]
         public void testOfConstructorn()
         {
             Triangle test1 = new Triangle(1.5, 2, 3);
         
         }
-
-
-
-
+        */
 
         [TestMethod]
-        public void isScaleneTest()    //Inga lika sidor
+        public void isScaleneTest1()    //Inga lika sidor
         {
             //Skickar in tre olika och ska få true tillbaka
 
-            Triangle treeSameTri = new Triangle(2.5, 3.5, 4.5);
-            Assert.IsFalse(treeSameTri.isScalene());
-            Assert.IsTrue(treeSameTri.isScalene());
-            Assert.AreSame(treeSameTri.isScalene());
-
+            Triangle triangle = new Triangle(2.5, 3.5, 4.5);           
+            Assert.IsFalse(triangle.isScalene());
+            
+            //Returnerar false!
         }
 
         [TestMethod]
-
-        public void isEquilateral()   //Liksidig
+        public void isScaleneTest2()    //Inga lika sidor
         {
-        
+            //Skickar in likbent för att få false tillbaka
+
+            Triangle triangle = new Triangle(2.5, 2.5, 4.5);
+            Assert.IsFalse(triangle.isScalene());
+
+            //Returnerar true!
         }
+
+        [TestMethod]
+        public void isScaleneTest3()    //Inga lika sidor
+        {
+            //Skickar in liksidig för att få false tillbaka
+
+            Triangle triangle = new Triangle(2.5, 2.5, 2.5);
+            Assert.IsTrue(triangle.isScalene());
+
+            //Returnerar true!
+        }
+
 
 
         [TestMethod]
-        public void isIsoscelesTest()    //Likbent 
+        public void isEquilateral1()   //Liksidig
         {
-            Triangle tri = new Triangle(1.0, 0.5, 1.0);
-            Assert.IsTrue(tri.isIsosceles());
+            //Skickar in liksidig för att få true tillbaka
+
+            Triangle triangle = new Triangle(2.5, 2.5, 2.5);
+            Assert.IsTrue(triangle.isScalene());
+
+            //Returnerar true!
         }
+
+        [TestMethod]
+        public void isEquilateral2()   //Liksidig
+        {
+            //Skickar in likbent för att få false tillbaka
+
+            Triangle triangle = new Triangle(2.5, 2.5, 4.5);
+            Assert.IsFalse(triangle.isScalene());
+
+            //Returnerar false!
+        }
+
+        [TestMethod]
+        public void isEquilateral3()   //Liksidig
+        {
+            //Skickar in olika sidor för att få false tillbaka
+
+            Triangle triangle = new Triangle(2.5, 3.5, 4.5);
+            Assert.IsFalse(triangle.isScalene());
+
+            //Returnerar false!
+        }
+
+
 
         
     }
