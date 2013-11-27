e4c58696f942e6d018ed133f783351acf67a71aa
mc22ft <mc22ft@student.lnu.se>
2013-11-27 11:06:52 +0100

    lagt till c# koden på brjat lite

---
+++ Whtiebox Test kod/UnitTestTry/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestTry
+{
+    [TestClass]
+    public class TriagleUnitTest1
+    {
+
+        [TestMethod]
+        public void testOfConstructorn()
+        {
+            Triangle test1 = new Triangle(1.5, 2, 3);
+        
+        }
+
+
+
+
+
+        [TestMethod]
+        public void isScaleneTest()    //Inga lika sidor
+        {
+            //Skickar in tre olika och ska få true tillbaka
+
+            Triangle treeSameTri = new Triangle(2.5, 3.5, 4.5);
+            Assert.IsFalse(treeSameTri.isScalene());
+            Assert.IsTrue(treeSameTri.isScalene());
+            Assert.AreSame(treeSameTri.isScalene());
+
+        }
+
+        [TestMethod]
+
+        public void isEquilateral()   //Liksidig
+        {
+        
+        }
+
+
+        [TestMethod]
+        public void isIsoscelesTest()    //Likbent 
+        {
+            Triangle tri = new Triangle(1.0, 0.5, 1.0);
+            Assert.IsTrue(tri.isIsosceles());
+        }
+
+        
+    }
+}
